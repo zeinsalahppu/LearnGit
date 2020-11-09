ed55710278bc34a2793b0a6b77a63c9505f02011
zeinsalahppu <zeinsalah@gmail.com>
2020-11-09 08:43:25 +0200

    defined functions addAtBeginning addAtEnd and printOut

--- LearnGit/Source.cpp
+++ LearnGit/Source.cpp
@@ -5,9 +5,18 @@ using namespace std;
 
 class SinglyLinkedList
 {
-public:
-  SinglyLinkedList();
-  ~SinglyLinkedList();
+  public:
+    SinglyLinkedList();
+    ~SinglyLinkedList();
+
+    void addAtBeginning(int n);
+    void addAtEnd(int n);
+    void insertIntoSortedList(int n);
+    void readAndCreate(bool sorted = false);
+    void printOut();
+    bool isSorted();
+    int getSize();
+
 
 private:
   struct Node
@@ -34,13 +43,61 @@ SinglyLinkedList::~SinglyLinkedList()
 }
 
 
+void SinglyLinkedList::addAtBeginning(int n)
+{
+  NodePTR x = new Node;
+  x->value = n;
+  x->next = m_Head;
+  m_Head = x;
+}
+
+
+void SinglyLinkedList::addAtEnd(int n)
+{
+  NodePTR x = new Node;
+  x->value = n;
+  x->next = NULL;
+
+  if (m_Head == NULL)
+    m_Head = x;
+  else
+  {
+    NodePTR t = m_Head;
+    while (t->next != NULL)
+      t = t->next;
+
+    t->next = x;
+  }
+}
+
+
+void SinglyLinkedList::printOut()
+{
+  NodePTR x = m_Head;
+  cout << "\n";
+  while (x != NULL)
+  {
+    cout << x->value << "  ";
+    x = x->next;
+  }
+}
+
 
 void main()
 {
   cout << "Hello! This is an example program for studying linked lists" << endl;
   SinglyLinkedList list;
 
+  list.addAtBeginning(5);
+  list.addAtBeginning(8);
+  list.addAtBeginning(12);
 
+  list.addAtEnd(15);
+  list.addAtEnd(16);
+  list.addAtEnd(17);
+  list.addAtEnd(18);
+
+  list.printOut();
 
 
   system("PAUSE");
